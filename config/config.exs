b988715227a7730fa59138ff506f3d0b9d9743bb
Colin Chauche <colin.chauche@gmail.com>
2022-01-23 17:16:55 -0500

    Remove node from build and fix junit config

--- config/config.exs
+++ config/config.exs
@@ -57,8 +57,3 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
-
-# Configure JUnit Formatter to output test results to a custom dir
-config :junit_formatter,
-  report_dir: Path.expand("_build/test/lib/acortador"),
-  prepend_project_name?: true
